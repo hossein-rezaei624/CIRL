603e10afc603ac24405ea649aa7de8b4f5c0722a
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-01-27 00:11:25 +0330

    KL added

--- utils/tools.py
+++ utils/tools.py
@@ -166,11 +166,14 @@ def factorization_loss(f_a, f_b):
     #f_a_norm = torch.Tensor(cluster_based(f_a.cpu().detach().numpy(),1,1))
     #f_b_norm = torch.Tensor(cluster_based(f_b.cpu().detach().numpy(),1,1))
 
-    c_ = torch.mm(f_a_norm.T, f_b_norm) / f_a_norm.size(0)
-    c = torch.Tensor(cluster_based(c_.cpu().detach().numpy(),1,1))
+    c = torch.mm(f_a_norm.T, f_b_norm) / f_a_norm.size(0)
+    #c = torch.Tensor(cluster_based(c_.cpu().detach().numpy(),1,1))
+
+    element_wise = 0.5 * (0 - torch.log(c.std()) + c.std() / 1 + (c.mean() - 0).pow(2) / 1 - 1)
+    kl_1 = element_wise.sum(-1)
 
     on_diag = torch.diagonal(c).add_(-1).pow_(2).mean()
     off_diag = off_diagonal(c).pow_(2).mean()
     loss = on_diag + 0.005 * off_diag
 
-    return loss
+    return loss + kl_1
