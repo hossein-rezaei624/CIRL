ad2586223006eb4b42a441f673352cfcec456870
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-01-27 21:48:15 +0330

    updated11

--- models/ResNet.py
+++ models/ResNet.py
@@ -38,15 +38,10 @@ class ResNet(nn.Module):
         v = ViT('B_16_imagenet1k',
         pretrained = True,
         image_size = 14,
-        patch_size = 2,
+        patches = 2,
         num_classes = 2048,
         dim = 2048,
-        depth = 6,
-        heads = 16,
-        mlp_dim = 2048,
-        channels = 1024,
-        dropout = 0.1,
-        emb_dropout = 0.1
+        in_channels = 1024,
         ).to("cuda")
 
 
