12d2c0d048e7b29e00c4319b50badf711a8f6fe7
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-01-27 17:27:46 +0330

    res

--- models/ResNet.py
+++ models/ResNet.py
@@ -56,7 +56,8 @@ class ResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.layer4(x)
+        print(x.shape)
+        # x = self.layer4(x)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
