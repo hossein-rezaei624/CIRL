67a32e8cc34e37b44ecaa417dbfddb3f530d4ff7
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-02-11 21:39:13 +0330

    newww

--- train.py
+++ train.py
@@ -96,6 +96,7 @@ class Trainer:
 
             ## --------------------------step 1 : update G and C -----------------------------------
             features = self.encoder(batch)
+            features = features + torch.randn((64,512)).to(self.device)
             masks_sup = self.masker(features.detach())
             masks_inf = torch.ones_like(masks_sup) - masks_sup
             if self.current_epoch <= 5:
@@ -149,6 +150,7 @@ class Trainer:
             ## ---------------------------------- step2: update masker------------------------------
             self.masker_optim.zero_grad()
             features = self.encoder(batch)
+            features = features + torch.randn((64,512)).to(self.device)
             masks_sup = self.masker(features.detach())
             masks_inf = torch.ones_like(masks_sup) - masks_sup
             features_sup = features * masks_sup
