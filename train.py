cbe6c8f59234bea60d968f3cfe80e5415e517da1
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-02-12 01:32:58 +0330

    asd

--- train.py
+++ train.py
@@ -96,6 +96,11 @@ class Trainer:
 
             ## --------------------------step 1 : update G and C -----------------------------------
             features = self.encoder(batch)
+            xxx = torch.randn(1)[0].to(self.device)
+            print("xxx",xxx)
+            print("features before",features)
+            features = features + xxx
+            print("features after",features)
             masks_sup = self.masker(features.detach())
             masks_inf = torch.ones_like(masks_sup) - masks_sup
             if self.current_epoch <= 5:
@@ -103,6 +108,10 @@ class Trainer:
                 masks_inf = torch.ones_like(features.detach())
             features_sup = features * masks_sup
             features_inf = features * masks_inf
+            #print("shape of sup:",features_sup.shape)
+            #print("shape of inf:", features_inf.shape)
+            #features_sup = features_sup + torch.randn((64,512)).to(self.device)
+            #features_inf = features_inf + torch.randn((64,512)).to(self.device)
             scores_sup = self.classifier(features_sup)
             scores_inf = self.classifier_ad(features_inf)
 
@@ -122,7 +131,7 @@ class Trainer:
             loss_dict["inf"] = loss_cls_inf.item()
             correct_dict["inf"] = calculate_correct(scores_inf, labels)
             num_samples_dict["inf"] = int(scores_inf.size(0))
-            
+
             # factorization loss for features between ori and aug
             loss_fac = factorization_loss(features_ori,features_aug)
             loss_dict["fac"] = loss_fac.item()
@@ -149,10 +158,15 @@ class Trainer:
             ## ---------------------------------- step2: update masker------------------------------
             self.masker_optim.zero_grad()
             features = self.encoder(batch)
+            features = features + torch.randn((64,512)).to(self.device)
             masks_sup = self.masker(features.detach())
             masks_inf = torch.ones_like(masks_sup) - masks_sup
             features_sup = features * masks_sup
             features_inf = features * masks_inf
+
+            #features_sup = features_sup + torch.randn((64,512)).to(self.device)
+            #features_inf = features_inf + torch.randn((64,512)).to(self.device)
+            
             scores_sup = self.classifier(features_sup)
             scores_inf = self.classifier_ad(features_inf)
 
