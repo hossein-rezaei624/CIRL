5ef910e1f518a2b41913e985e0df66c3b370bcf1
Hossein Rezaei <hossein.rezaei624@gmail.com>
2023-02-12 04:36:26 +0330

    dewqd

--- models/classifier.py
+++ models/classifier.py
@@ -9,11 +9,13 @@ class Classifier(nn.Module):
         self.in_dim = in_dim
         self.num_classes = num_classes
 
-        self.layers = nn.Linear(in_dim, num_classes)
+        self.layers = nn.Sequential(
+            nn.Linear(in_dim, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, num_classes))
 
     def forward(self, features):
         scores = self.layers(features)
-        #print("Scores:",scores)
         return scores
 
 
